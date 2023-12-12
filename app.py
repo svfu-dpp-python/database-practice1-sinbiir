24b623f7cef573e6604ec0b1d3e21fe1ee5b8a85
Владимир Леверьев <vs.leverev@s-vfu.ru>
2023-12-12 15:14:50 +0900

    Завершить текст задания

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from mysql.connector import connect
 
 
-def get_cities():
+def get_data(table):
+    """Получение данных из СУБД MySQL"""
     conn = connect(host="localhost", user='root', password='1234', database='world')
     cursor = conn.cursor()
-    query = "SELECT * FROM `city` LIMIT 25;"
+    query = f"SELECT * FROM `{table}` LIMIT 25;"  # читаем 25 строк из таблицы
     cursor.execute(query)
     result = cursor.fetchall()
     cursor.close()
@@ -15,10 +16,12 @@ def get_cities():
 
 class CustomHttpRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self) -> None:
+        """Отправка веб-страницы"""
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        cities = [f'<li>{c[1]}</li>' for c in get_cities()]
+        rivers = [f'<li>{c[1]}</li>' for c in get_data('river')]
+        cities = [f'<li>{c[1]}</li>' for c in get_data('city')]
         html = f'''
         <html>
             <head>
@@ -26,6 +29,10 @@ class CustomHttpRequestHandler(BaseHTTPRequestHandler):
                 <title>HTTP-сервер</title>
             </head>
             <body>
+                <h1>Реки</h1>
+                <ul>
+                    {''.join(rivers)}
+                </ul>
                 <h1>Города</h1>
                 <ul>
                     {''.join(cities)}
@@ -37,6 +44,7 @@ class CustomHttpRequestHandler(BaseHTTPRequestHandler):
 
 
 def run_server():
+    """Запуск веб-сервера"""
     server_address = ('', 8000)
     httpd = HTTPServer(server_address, CustomHttpRequestHandler)
     httpd.serve_forever()
